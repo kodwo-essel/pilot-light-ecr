a34f4cfa39a904bcc541606e3f860b4ba1d93db6
Jimmy Essel <jimmykodwoessel@gmail.com>
2025-04-28 16:24:40 +0000

    chore: removed duplicate data

--- modules/ecr_replication/outputs.tf
+++ modules/ecr_replication/outputs.tf
@@ -1,6 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-
 output "replica_repository_url" {
   value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.replica_region}.amazonaws.com/${var.prefix_filter}"
 }
